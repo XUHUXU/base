894b2c776a297f9b4ab574ef66410655886870ef
XUHU <email.xuhuxu.xyz>
2020-08-04 23:01:02 +0800

    [2020-08-04]初识Java语言

---
+++ src/one_language_overview/FirstJava.java
@@ -0,0 +1,24 @@
+package one_language_overview;
+
+/**
+ * @program: base
+ * @description: 源文件中包含多个类
+ * @author: xuhu
+ * @create: 2020-08-04 21:34
+ **/
+public class FirstJava {
+}
+
+/*
+    一个Java源文件可以包含多个类定义，但最多只能包含一个public类定义;
+    如果Java源文件包含public类定义，则源文件的文件名与public类名保持一致
+*/
+
+/*
+public class SecondeJava{
+}
+*/
+
+
+class ThirdJava{
+}
---
+++ src/one_language_overview/TestGc.java
@@ -0,0 +1,16 @@
+package one_language_overview;
+
+/**
+ * @program: base
+ * @description: 垃圾回收机制
+ * @author: xuhu
+ * @create: 2020-08-04 22:45
+ **/
+public class TestGc {
+    public static void main(String[] args) {
+        /*
+        *   建议系统进行垃圾回收，但这种调用只是建议，不能精确控制垃圾回收机制的执行
+        */
+        System.gc();
+    }
+}
