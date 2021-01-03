e2b84ce087533f9538b00556aed372ce6a41621f
XUHU <email.xuhuxu.xyz>
2021-01-03 18:55:25 +0800

    [2021-01-03]字符类型

---
+++ src/1_data_type/TestChar.java
@@ -0,0 +1,17 @@
+/**
+ * @program: base
+ * @description: 数据类型：Char
+ * @author: xuhu
+ * @create: 2021-01-03 16:29
+ **/
+public class TestChar {
+    public static void main(String[] args) {
+       /*
+          字符类型数据不能赋值为"\'\'"空字符
+            char a = '';
+          单引号之间有且只有一个字符，没有不行，有两个也不行
+       */
+       char a = ' ';
+        System.out.println("==="+a+"===");
+    }
+}
