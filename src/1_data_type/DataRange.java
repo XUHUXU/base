e53cd010665e8941c9835af974275ef63088a997
XUHU <email.xuhuxu.xyz>
2021-01-10 13:56:41 +0800

    [2021-01-10]数据类型

---
+++ src/1_data_type/DataRange.java
@@ -0,0 +1,22 @@
+/**
+ * @program: base
+ * @description: 数据范围
+ * @author: xuhu
+ * @create: 2021-01-10 13:30
+ **/
+public class DataRange {
+    public static void main(String[] args) {
+        //  byte b2= 128;  整数默认数据类型为int,程序看到128超过byte类型数据范围，不能直接赋值。127可以直接认定为byte类型。
+        byte b1= 127;
+
+        long l1 = 111111;
+        // long l2 = 11111111111; 默认数据类型为int,超过数据范围不合理,因此报错
+        long l2 = 11111111111L; //  使用“l”,“L”都可以,“L”便于与“1”区分
+
+        /*
+           数据“1.2”默认类型是double类型，double类型数据类型大于float类型，因此报错
+           float f = 1.2;
+        */
+        float f = 1.2F;
+    }
+}
--- src/1_data_type/DataType.java
+++ src/1_data_type/DataType.java
@@ -1,17 +1,23 @@
 /**
  * @program: base
- * @description: 数据类型：Char
+ * @description: 数据类型
  * @author: xuhu
- * @create: 2021-01-03 16:29
+ * @create: 2021-01-10 13:50
  **/
-public class TestChar {
+public class DataType {
     public static void main(String[] args) {
        /*
           字符类型数据不能赋值为"\'\'"空字符
             char a = '';
           单引号之间有且只有一个字符，没有不行，有两个也不行
        */
-       char a = ' ';
+        char a = ' ';
         System.out.println("==="+a+"===");
+
+        /*
+            boolean类型为逻辑数据类型,只能为true、flase
+            boolean b2 = null; 因此报错
+        */
+        boolean b = true;
     }
 }
