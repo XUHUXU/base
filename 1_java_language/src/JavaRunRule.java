8e27ff5e05c3c662dff0de9fe3fe9864942b07c7
XUHU <email.xuhuxu.xyz>
2021-08-08 18:51:11 +0800

    [2021-08-08]Java运行入口

---
+++ 1_java_language/src/JavaRunRule.java
@@ -0,0 +1,20 @@
+/**
+ * @program: base
+ * @description: Java运行规则
+ * @author: xuhu
+ * @create: 2021-06-07 06:57
+ **/
+public class JavaRunRule {
+    /*
+        Java解释器规定，如果某个类能被解释器直接执行，则这个类里必须包含main方法，且main方法必须使用public static修饰，必须使
+        void声明该方法的返回值，且main方法的形参必须使字符串数组类型（String[] args是字符串数据的形式），也就是说main方法是固
+        定的。 因此main方法是Java程序的入口
+    */
+    public static void main(String[] args) {
+        
+    }
+}
+
+class JavaRule2{
+
+}
