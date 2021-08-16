10505ff6e0ca1ab3a9724218ef12361c18f776f8
XUHU <email.xuhuxu.xyz>
2021-08-17 00:03:55 +0800

    [2021-08-16] javadoc生成API文档

---
+++ 1_java_language/src/API/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"l":"main(String[])","h":"","u":"main(java.lang.String[])"},{"l":"TestAnnotation()","h":"","u":"%3Cinit%3E()"},{"l":"testInt()","h":"","u":""},{"l":"testProtectde()","h":"","u":""}]
